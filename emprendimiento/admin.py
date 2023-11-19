ab42921d5497a4dfdbb02983c65bb57044586865
galmarcano <genesismarc@gmail.com>
2023-11-19 19:24:34 -0300

    backend login, select_user, registro_usuario_form

--- emprendimiento/admin.py
+++ emprendimiento/admin.py
@@ -1,20 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Emprendedor, Emprendimiento, Producto, Servicio, Comentario
+from .models import Emprendimiento, Producto, Comentario
 
-class EmprendedorAdmin(admin.ModelAdmin):
-    list_display = ('id_emprendedor', 'nro_identificacion_emprendedor', 
-                   'nombre_emprendedor', 'apellido_emprendedor', 
-                   'email_emprendedor', 'telefono_emprendedor', 
-                   'id_emprendimiento')
-    
-    ordering = ('id_emprendedor', )
 
-    list_display_links = ('id_emprendedor', 'nombre_emprendedor')
-
-    list_filter = ('id_emprendedor', 'nombre_emprendedor', 'id_emprendimiento')
-admin.site.register(Emprendedor, EmprendedorAdmin)
 
 class EmprendimientoAdmin(admin.ModelAdmin):
     list_display = ('id_emprendimiento', 'nombre_emprendimiento',
@@ -42,18 +31,7 @@ class ProductoAdmin(admin.ModelAdmin):
     list_filter = ('id_producto', 'codigo_producto', 'id_emprendimiento')
 admin.site.register(Producto, ProductoAdmin)
 
-class ServicioAdmin(admin.ModelAdmin):
-    list_display = ('id_servicio', 'codigo_servicio',
-                   'nombre_servicio', 'descripcion_servicio', 
-                   'precio_servicio', 'disponibilidad_servicio',
-                   'id_emprendimiento')
-    
-    ordering = ('id_servicio', )
 
-    list_display_links = ('id_servicio', 'codigo_servicio')
-
-    list_filter = ('id_servicio', 'codigo_servicio', 'id_emprendimiento')
-admin.site.register(Servicio, ServicioAdmin)
 
 class ComentarioAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'apellido', 'correo', 'mensaje')
