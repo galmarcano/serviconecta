694dd1d04da3803346ef1fbd33f71258be261371
galmarcano <genesismarc@gmail.com>
2023-11-24 19:21:23 -0300

    wsgi

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.middleware import WhiteNoiseMiddleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
 application = WhiteNoiseMiddleware(application)
